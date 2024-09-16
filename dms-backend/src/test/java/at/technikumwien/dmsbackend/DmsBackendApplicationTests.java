90b11b458fce49db6ec82558c8fbfcf3eb0333a9
Emil Auer <e.auer@itkaufmann.at>
2024-09-16 19:01:01 +0200

    Init backend

---
+++ dms-backend/src/test/java/at/technikumwien/dmsbackend/DmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikumwien.dmsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
